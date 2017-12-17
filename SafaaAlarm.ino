c50d53289f7c34d71f1c9736fd70ae2429caf781
Steven Youhana <stevenyouhana@hotmail.co.nz>
2017-12-17 17:35:18 +1300

    testing stage, interrupts might not be the way to go

--- SafaaAlarm.ino
+++ SafaaAlarm.ino
@@ -1,14 +1,19 @@
 int ledBlue = 4;
 int ledRed = 5;
-int zoneLEDs[] = {6,7,8};
-int sensors[] = {9,10,11};  //INPUTS
+/*
+ * A constant expression is an expression with a value that is determined during compilation.
+ * That value can be evaluated at runtime, but cannot be changed.
+ */
+constexpr int zoneLEDs[] = {6,7,8};
+constexpr int sensors[] = {9,10,11};  //INPUTS
 const int alarm = 2;  //interrupt?
-const int ALARM_DURATION = 60000;
+const int ALARM_DURATION = 3000; //ALTER TIME
 boolean ringing = false;
-const int flashSpeed = 200; //POLICE LIGHTS
+const int flashSpeed = 50; //POLICE LIGHTS
 
-void setup() {
+void setup(){
   Serial.begin(9600);
+  pinMode(3,INPUT);
   //police lights
   pinMode(ledBlue,OUTPUT);
   pinMode(ledRed,OUTPUT);
@@ -22,30 +27,72 @@ void setup() {
   pinMode(sensors[2],INPUT);
   //alarm
   pinMode(alarm,OUTPUT);  //check if INPUT_PULLUP
-  attachInterrupt(digitalPinToInterrupt(1),policeLights,HIGH);
+  Serial.println("Start...");
+  attachInterrupt(digitalPinToInterrupt(3),policeLights,HIGH);
 }
 
 void loop(){
+  //testLEDs();
+  //testButtons();
   switchZoneLED(getFiredSensor());
   ringAlarm();
-}
 
+  delay(10);
+}
+void testButtons(){
+  if(digitalRead(9) == HIGH){
+    Serial.println("ON 9");
+  }
+  if(digitalRead(10) == HIGH){
+    Serial.println("ON 10");
+  }
+  if(digitalRead(11) == HIGH){
+    Serial.println("ON 11");
+  }
+  
+  else{
+    Serial.println("OFF");
+  }
+}
+void testLEDs(){
+  for(int i=2; i<9; i++){
+    digitalWrite(i,HIGH);
+    Serial.println(i);
+    delay(200);
+    digitalWrite(i,LOW);
+    delay(200);
+  }
+}
 int getFiredSensor(){
-  int count = 0;
+  Serial.println("gettingFired");
   while(!(ringing)){
-    if(sensors[count] == HIGH){
-      ringing = true; //RINGING TRUE
-      return count;
+    Serial.println("pass IF");
+    for(int i=0; i<3; i++){
+      if(digitalRead(sensors[i]) == HIGH){
+        ringing = true; //RINGING TRUE
+        Serial.println("SENSOR OFF");
+        return sensors[i];
+      }
     }
   }
 }
 
+
 void switchZoneLED(int led){
-  digitalWrite(led,HIGH);
+  switch(led){
+    case sensors[0]: digitalWrite(zoneLEDs[0],HIGH);
+    break;
+    case sensors[1]: digitalWrite(zoneLEDs[1],HIGH);
+    break;
+    case sensors[2]: digitalWrite(zoneLEDs[2],HIGH); 
+    break;
+  }
 }
 
 void systemReset(){
+  Serial.println("ACCESS RES");
   ringing = false;
+  digitalWrite(3,LOW);
   digitalWrite(alarm,LOW);
   //reset OUTPUT pins
   for(int i=4; i<9; i++){
@@ -57,21 +104,22 @@ void policeLights(){
   if(ringing){
     for(int i=0; i<6; i++){
       digitalWrite(ledRed,HIGH);
-      delay(flashSpeed);
+      //delay(flashSpeed);
       digitalWrite(ledRed,LOW);
-      delay(flashSpeed);
+      //delay(flashSpeed);
     }
     for(int i=0; i<6; i++){
       digitalWrite(ledBlue,HIGH);
-      delay(flashSpeed);
+      //delay(flashSpeed);
       digitalWrite(ledBlue,LOW);
-      delay(flashSpeed);
+      //delay(flashSpeed);
     }
   }
 }
 
 void ringAlarm(){
-  while(ringing){
+  digitalWrite(3,HIGH);
+  if(ringing){
     digitalWrite(alarm,HIGH);
   }
   delay(ALARM_DURATION);
