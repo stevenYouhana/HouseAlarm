068f27a2186a7cd7fc22c6ad55ea079cc3b31064
Steven Youhana <stevenyouhana@hotmail.co.nz>
2017-12-23 19:02:36 +1300

    developed so the triggered one led stays on after systemReset()

--- SafaaAlarm.ino
+++ SafaaAlarm.ino
@@ -10,6 +10,7 @@ unsigned long margin = 0;
 //police lights
 int flashSpeed = 50;
 const int alarm = 2;
+boolean onZones[] = {false,false,false};
 boolean ringing = false;
 const long ALARM_DURATION = 5000; //ALTER TIME
 boolean trip = false; //used to set the final time recorded for margin as a reference point
@@ -29,17 +30,16 @@ void setup(){
   pinMode(sensors[2],INPUT);
   //alarm
   pinMode(alarm,OUTPUT);
-  margin = 0;
   Serial.println("Start...");
 }
 
 void loop(){
   //testLEDs();
   //testButtons();
-  testTimer();
-  //delay(10);
+  mainOperation();
+  DEVELOPMENT_RESET();
 }
-void testTimer(){
+void mainOperation(){
   unsigned long runTime = millis();
   switchZoneLED(getFiredSensor());
   setMargin();
@@ -59,7 +59,7 @@ void setMargin(){
 
 int getFiredSensor(){
   if(!(ringing)){
-    Serial.println("gettingFired");
+    Serial.println("standing by...");
     for(int i=0; i<3; i++){
       if(digitalRead(sensors[i]) == HIGH){
         ringing = true; //RINGING TRUE
@@ -72,13 +72,13 @@ int getFiredSensor(){
 }
 
 void switchZoneLED(int led){
-  if((ringing)){
+  if(ringing){
     switch(led){
-      case sensors[0]: digitalWrite(zoneLEDs[0],HIGH);
+      case sensors[0]: digitalWrite(zoneLEDs[0],HIGH); onZones[0] = true;
       break;
-      case sensors[1]: digitalWrite(zoneLEDs[1],HIGH);
+      case sensors[1]: digitalWrite(zoneLEDs[1],HIGH); onZones[1] = true;
       break;
-      case sensors[2]: digitalWrite(zoneLEDs[2],HIGH); 
+      case sensors[2]: digitalWrite(zoneLEDs[2],HIGH); onZones[2] = true;
       break;
     }
   }
@@ -115,23 +115,37 @@ void systemReset(){
   trip = false;
   digitalWrite(alarm,LOW);
   //reset OUTPUT pins
-  for(int i=4; i<9; i++){
-      digitalWrite(i,LOW);
-  }
+  digitalWrite(alarm,LOW);
+  digitalWrite(ledRed,LOW);
+  digitalWrite(ledBlue,LOW);
   Serial.println("SYSTEM RESETING...");
   delay(2000);
-  systemRestart();
+  systemRestart();  //NOTIFYER
 }
 void systemRestart(){
-  for(int i=0; i<4; i++){
-    digitalWrite(6,HIGH);
-    digitalWrite(7,HIGH);
-    digitalWrite(8,HIGH);
-    delay(200);
-    digitalWrite(6,LOW);
-    digitalWrite(7,LOW);
-    digitalWrite(8,LOW);
-    delay(200);
+   for(int i=0; i<4; i++){
+        digitalWrite(zoneLEDs[0],HIGH);
+        digitalWrite(zoneLEDs[1],HIGH);
+        digitalWrite(zoneLEDs[2],HIGH);
+        delay(200);
+        digitalWrite(zoneLEDs[0],LOW);
+        digitalWrite(zoneLEDs[1],LOW);
+        digitalWrite(zoneLEDs[2],LOW);
+        delay(200);
+    }
+  for(int on=0; on<3; on++){
+    Serial.print(onZones[on]);
+    if(onZones[on]){
+      digitalWrite(on+zoneLEDs[0],HIGH);
+    }
+  }
+}
+
+void DEVELOPMENT_RESET(){
+  if(sensors[0]==HIGH & sensors[1]==HIGH & sensors[2]==HIGH){
+    digitalWrite(zoneLEDs[0],LOW);
+    digitalWrite(zoneLEDs[1],LOW);
+    digitalWrite(zoneLEDs[2],LOW);
   }
 }
 
